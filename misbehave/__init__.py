266ca80e1df24fac8913fee850e2983cf6a7a7cf
Piper Thunstrom <pathunstrom@gmail.com>
2020-05-27 23:28:42 -0400

    Module for decorators.

--- misbehave/__init__.py
+++ misbehave/__init__.py
@@ -2,67 +2,7 @@ from time import perf_counter
 from typing import Any, List
 
 
-class Decorator(Node):
-    """
-    Adds functionality similarly to a decorator function.
 
-    Subclass and do work before and after the call.
-    """
-    def __init__(self, child, **_):
-        self.child = child
-
-    def visit(self, actor: Any, context: Any) -> State:
-        return self.child.visit(actor, context)
-
-    def reset(self):
-        self.state = State.READY
-        self.child.reset()
-
-
-class Inverter(Decorator):
-    """
-    Changes Success to Failure and vice versa
-    """
-
-    def visit(self, actor: Any, context: Any) -> State:
-        result = super().visit(actor, context)
-        if result is State.SUCCESS:
-            return State.FAILED
-        elif result is State.FAILED:
-            return State.SUCCESS
-        else:
-            return result
-
-
-class ThrowEventOnSuccess(Decorator):
-
-    def __init__(self, child, *, event_type, get_event_params):
-        super().__init__(child)
-        self.event_type = event_type
-        self.get_event_params = get_event_params
-
-    def visit(self, actor: Any, context: Any) -> State:
-        result = self.child.visit(actor, context)
-        if result is State.SUCCESS:
-            context.signal(self.event_type(*self.get_event_params(actor)))
-        return result
-
-
-class Debounce(Decorator):
-
-    def __init__(self, child, *, cool_down=0.5, timer=perf_counter):
-        super().__init__(child)
-        self.cool_down = cool_down
-        self.timer = timer
-        self.attr = f"debounce_{id(self)}_last"
-
-    def visit(self, actor: Any, context: Any) -> State:
-        if self.timer() <= getattr(actor, self.attr, -10) + self.cool_down:
-            return State.FAILED
-        result = self.child.visit(actor, context)
-        if result is State.SUCCESS:
-            setattr(actor, self.attr, self.timer())
-        return result
 
 
 class CheckValue(Node):
---
+++ misbehave/decorator.py
@@ -0,0 +1,51 @@
+from time  import perf_counter
+from typing import Any
+
+from misbehave.common import BaseNode, State
+
+
+class Decorator(BaseNode):
+    """
+    Adds functionality similarly to a decorator function.
+
+    Subclass and do work before or after the call.
+    """
+    def __init__(self, child, **_):
+        self.child = child
+
+    def __call__(self, actor: Any, context: Any) -> State:
+        return self.child.visit(actor, context)
+
+
+class Inverter(Decorator):
+    """
+    Changes Success to Failure and vice versa
+    """
+
+    def __call__(self, actor: Any, context: Any) -> State:
+        result = super().__call__(actor, context)
+        if result is State.SUCCESS:
+            return State.FAILED
+        elif result is State.FAILED:
+            return State.SUCCESS
+        else:
+            return result
+
+
+class Debounce(Decorator):
+    """
+    If child node is successful, only run again after a delay.
+    """
+    def __init__(self, child, *, delay=0.5, timer=perf_counter):
+        super().__init__(child)
+        self.cool_down = delay
+        self.timer = timer
+        self.attr = f"debounce_{id(self)}_last"
+
+    def __call__(self, actor: Any, context: Any) -> State:
+        if self.timer() <= getattr(actor, self.attr, -10) + self.cool_down:
+            return State.FAILED
+        result = self.child.visit(actor, context)
+        if result is State.SUCCESS:
+            setattr(actor, self.attr, self.timer())
+        return result
