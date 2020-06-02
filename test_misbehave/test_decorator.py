e1f60ec7c3f7b6de0223fdc4c2145f2d99e1163d
Piper Thunstrom <pathunstrom@gmail.com>
2020-06-01 20:23:09 -0400

    Adds test for Debounce.

--- test_misbehave/test_decorator.py
+++ test_misbehave/test_decorator.py
@@ -5,3 +5,27 @@ from misbehave.common import State
 def test_base_decorator():
     node = decorator.Decorator(lambda a, c: State.SUCCESS)
     assert isinstance(node, decorator.Decorator)
+
+
+def test_debounce():
+
+    times = (x for x in range(5))
+
+    def timer():
+        return next(times)
+
+    tree = decorator.Debounce(lambda a, c: State.SUCCESS, delay=1.5, timer=timer)
+
+    class Actor:
+        pass
+
+    test_actor = Actor()
+
+    result = tree(test_actor, None)
+    assert result is State.SUCCESS
+
+    result = tree(test_actor, None)
+    assert result is State.FAILED
+
+    result = tree(test_actor, None)
+    assert result is State.SUCCESS
